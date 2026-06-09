655aa4940dedffe50140b817721c014f3a723331
TigerCasteneda <TigerCasteneda@users.noreply.github.com>
2026-06-09 21:58:08 +0800

    fix: revert readOnly to !token

--- app/projects/[id]/essay/page.tsx
+++ app/projects/[id]/essay/page.tsx
@@ -142,7 +142,7 @@ function EssayPageContent() {
   const { ydoc, ytext, awareness, commentsMap } = useEssayCollab({
     fileId: fileId ?? filePath ?? "",
     initialContent: initialContent ?? undefined,
-    readOnly: !isTauri() && !token,
+    readOnly: !token,
     onSynced: () => setSyncState("synced"),
   })
 
@@ -278,7 +278,7 @@ function EssayPageContent() {
             ytext={ytext}
             awareness={awareness}
             commentsMap={commentsMap}
-            readOnly={!isTauri() && !token}
+            readOnly={!token}
             onChange={handleContentChange}
           />
         </Panel>
